805efa75da3970b9ed37ce03312d21d3ccdad1ca
Chanyeol Park <chanyeol@edu.hanbat.ac.kr>
2020-07-27 17:51:19 +0900

    CORS 적용

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
   devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    },
     disableHostCheck: true,
   },
 };
