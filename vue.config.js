0ce364911ff252ead87787ecea7539b178ec9816
전철호 <cheolho@wisoft.io>
2020-08-13 15:18:24 +0900

    modify mosca config

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,15 @@ module.exports = {
   transpileDependencies: ['vuetify'],
   devServer: {
     https: true,
+    https: {
+      key: fs.readFileSync('/home/ubuntu/mktop/keys/privkey.pem'),
+      cert: fs.readFileSync('/home/ubuntu/mktop/keys/cert.pem'),
+      ca: fs.readFileSync('/home/ubuntu/mktop/keys/fullchain.pem'),
+    },
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept',
     },
     disableHostCheck: true,
   },
